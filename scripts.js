e3bfe616f8daef90c1b485aaf1dddbf33a8759a7
VinayGohel <167532749+VinayGohel@users.noreply.github.com>
2024-07-23 17:10:24 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+document.getElementById('volunteer-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for signing up to volunteer!');
+    
+});
+
+document.getElementById('donate-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your donation!');
+    
+});
